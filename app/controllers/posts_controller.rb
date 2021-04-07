5a2250f0b75adaa19deb0f23f26be1c07ddb40d3
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-04-08 06:48:46 +0900

    詳細画面機能を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,16 @@
 class PostsController < ApplicationController
   def index
-  end
+    @posts = Post.all
+  end  
   def new
     @post = Post.new
   end
   def create
     Post.create(post_params)
     redirect_to new_post_path
+  end
+  def show
+    @post = Post.find(params[:id])
   end
   private
   def post_params
