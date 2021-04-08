557aae0b26109c5f0f7bd17ea3bb0fd68e3bd23b
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-04-08 09:20:19 +0900

    コントローラの共通処理

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
     @posts = Post.all
   end  
@@ -14,13 +15,10 @@ class PostsController < ApplicationController
     end
   end
   def show
-    @post = Post.find(params[:id])
   end
   def edit
-    @post = Post.find(params[:id])
   end
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to posts_path
     else
@@ -30,5 +28,8 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
