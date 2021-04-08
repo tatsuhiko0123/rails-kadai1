bd5f58d5d57c8edb3e5767398f7bd34a7450e851
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-04-08 09:31:52 +0900

    削除機能を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end  
@@ -24,6 +24,10 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+  def destroy
+    @post.destroy
+    redirect_to posts_path
   end
   private
   def post_params
